081f6a3e000e68b98c9f8e2dd39602b58bc972a7
AjeetTanwar <ajeet.tanwar95@gmail.com>
2024-06-10 12:10:41 +0530

    commit

---
+++ src/main/java/com/notification/ChatController.java
@@ -0,0 +1,28 @@
+package com.notification;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class ChatController {
+
+     @Autowired
+    SimpMessagingTemplate simpMessagingTemplate;
+
+    @MessageMapping("/application")
+    @SendTo("/all/messages")
+    public Message send(final Message message) throws Exception {
+        return message;
+    }
+
+    @MessageMapping("/private")
+    public void sendToSpecificUser(@Payload Message message) {
+        simpMessagingTemplate.convertAndSendToUser(message.getTo(), "/specific", message);
+    }
+
+}
